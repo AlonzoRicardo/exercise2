520d37897ceaf78e58169e256bebf18e508a6b1e
RicardoAlonzo <alonzoricardo6@gmail.com>
2018-10-30 15:28:48 +0100

    intenta romperlo cesar

--- index.js
+++ index.js
@@ -1,31 +1,36 @@
 const axios = require('axios');
-var express = require('express')
-var bodyParser = require('body-parser')
-var app = express()
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
 
+function test() {
+    return (req, res, next) => {
+
+        let { destination, body } = req.body
+        typeof req.body !== 'object' ?
+            res.send('expected payload of type object')
+            :
+            next()
+    }
+}
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-
-app.get('/', function (req, res) {
-    res.send('Hello World')
-})
-
-
-app.post('/message', (req, res) => {
+app.post('/message', test(), (req, res) => {
     let { destination, body } = req.body
-    axios.post('http://messageapp:3000/message', { destination, body })
-        .then(() => {
-            res.status(200).send('Succesful');
-        })
-        .catch((err) => {
 
-            console.log(typeof err);
-            res.status(500).send('error');
-        
-        })
+    if (destination.length < 100 && body.length < 100) {
+        axios.post('http://localhost:3000/message', { destination, body })
+            .then(() => {
+                res.status(200).send('Succesful');
+            })
+            .catch((err) => {
+                res.status(500).send('error');
+            })
+    } else {
+        res.send('Error body is too long')
+    }
 })
-
 
 app.listen(9001)
